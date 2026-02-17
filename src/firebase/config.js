301a5634d6353f48bd39789ad9c8969db2c094ed
Achintya Singh <achintyasingh48@gmail.com>
2026-02-17 15:59:37 +0530

    update rulebook

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMDaA7hco4hNXzphZa62EkJ2E-3qKq5zE",
-  authDomain: "gec-fresher-party.firebaseapp.com",
-  projectId: "gec-fresher-party",
-  storageBucket: "gec-fresher-party.firebasestorage.app",
-  messagingSenderId: "473377324797",
-  appId: "1:473377324797:web:7d62e990a1a70b6a4e8894"
+  apiKey: "AIzaSyDaunaGODx0mGSgCSY6wdaf_Guets3ELnU",
+  authDomain: "event-9ebcd.firebaseapp.com",
+  projectId: "event-9ebcd",
+  storageBucket: "event-9ebcd.firebasestorage.app",
+  messagingSenderId: "747181611512",
+  appId: "1:747181611512:web:48f34fbfa2fc9d538e5e0d",
 };
 
 // Initialize Firebase
