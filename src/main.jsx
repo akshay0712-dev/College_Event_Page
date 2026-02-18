3436662f12ef73800edcda0779e1f07a1fb9d2d9
Aryaswapnil07 <achintyasingh48@gmail.com>
2026-02-18 19:28:01 +0530

    configure main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import emailjs from '@emailjs/browser'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// ✅ Initialize EmailJS once at app startup
+emailjs.init('owVDz9Fo3FQoYw3_x')
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
